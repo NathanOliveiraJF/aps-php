7bae572dca96442251e71b8525b941a5106b2a31
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-23 22:08:43 -0300

    add exercicio resvolido e)

---
+++ e/App/Models/Pessoa.php
@@ -0,0 +1,17 @@
+<?php
+//PSR-2 :: Fechamento da tag php "?">" deve ser omitida caso tenha apenas codigo php
+
+
+// - PSR-4 :: Especificação para classes de carregamento automático a partir de caminhos de arquivo. 
+// - Um nome de classe totalmente qualificado tem o seguinte formato:
+// - \<NamespaceName>(\<SubNamespaceNames>)*\<ClassName>
+
+//Onde neste caso App -> fornecedor(vendor), Models -> subnamespace, Pessoa -> a classe
+namespace App\Models;
+
+//PSR-1 :: Nome de classes deve ser declarado em StudlyCaps
+class Pessoa 
+{
+  
+}
+
